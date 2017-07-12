8d9c575cbce7de55daa0034de75d37f791cb49ff
lwf <liwanfeng@yunkecn.com>
2017-07-12 17:31:50 +0800

    问题更改

--- src/main/java/com/lwf/springboot/controller/TestController.java
+++ src/main/java/com/lwf/springboot/controller/TestController.java
@@ -2,6 +2,7 @@ package com.lwf.springboot.controller;
 
 import com.lwf.springboot.model.User;
 import com.sun.deploy.net.HttpResponse;
+import com.sun.tracing.dtrace.Attributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lwf on 2017/3/1.
@@ -34,12 +37,36 @@ public class TestController {
         return users;
     }
 
+    @ResponseBody
     @RequestMapping("/adduser")
-    public int AddUser(User user, HttpServletRequest request) {  //, HttpServletResponse response
-        String userName = request.getParameter("userName");
-        //response.setStatus(1);
-        users.add(user);
-        return 1;
+    public Map AddUser(@RequestBody User user, HttpServletResponse response) {  //,
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        if (user.getUserName() != null) {
+            users.add(user);
+            resultMap.put("Code", "1");
+            resultMap.put("Msg", "success");
+        } else {
+            resultMap.put("Code", "0");
+            resultMap.put("Msg", "failer");
+        }
+        return resultMap;
+    }
+
+    @ResponseBody
+    @RequestMapping("/deluser")
+    public Map DelUser(@RequestBody User user, HttpServletResponse response) {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        if (user.getUserName() != null) {
+            users.removeIf(s -> s.getUserName().equals(user.getUserName()));
+            resultMap.put("Code", "1");
+            resultMap.put("Msg", "success");
+        } else {
+            resultMap.put("Code", "0");
+            resultMap.put("Msg", "failer");
+        }
+        return resultMap;
     }
 
     @RequestMapping("/test1")
--- src/main/java/com/lwf/springboot/controller/UserController.java
+++ src/main/java/com/lwf/springboot/controller/UserController.java
@@ -6,13 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.List;
 
 /**
