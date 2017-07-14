6870e9f9164ba9ae3a51276783cbd0932790464d
lwf <liwanfeng@yunkecn.com>
2017-07-14 14:41:49 +0800

    增加默认欢迎页

--- src/main/java/com/lwf/springboot/utils/GlobalExceptionHandler.java
+++ src/main/java/com/lwf/springboot/utils/GlobalExceptionHandler.java
@@ -2,14 +2,9 @@ package com.lwf.springboot.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
